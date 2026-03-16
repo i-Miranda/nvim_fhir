8bea65d2c428c0541f270defb774b1282e272df6
Ivan Miranda <ivanmirandaart@yahoo.com>
2026-03-16 15:44:11 +0100

    updated init.lua to fix lsp issues, also added fzf-lua instead of telescope. need to fix checkhealth dependency issues

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
--- Pon esto al principio del archivo
+-- 0. Variables de entorno y compatibilidad
 vim.env.CFLAGS = "-O0"
+vim.cmd([[ source ~/.config/nvim/vimrc ]])
 
--- Set rtp for vim flags
- vim.cmd([[ source vimrc ]])
-
--- 1. instalador de lazy.nvim
+-- 1. Instalador de lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
@@ -26,71 +24,54 @@ require("lazy").setup({
       })
     end
   },
+  { "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {} 
+    },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 })
 
--- 3. Configuración NATIVA de Neovim 0.11 (No más warnings)
-local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/"
+-- 3. Configuración LSPs (Neovim 0.11 Ready)
+local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/"
 
--- Función auxiliar para arrancar LSPs sin usar el framework deprecado
-local function setup_server(name, config)
-  local lsp_config = require('lspconfig.configs')[name]
-  if lsp_config then
-    lsp_config.setup(vim.tbl_extend("force", {
-      capabilities = capabilities,
-    }, config))
-  end
+-- Actualizamos el PATH del sistema para que Neovim encuentre los ejecutables de Mason
+vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
+
+--- Ayudante para configurar servidores de forma limpia
+local function setup(server, config)
+  config = config or {}
+  config.capabilities = capabilities
+  lspconfig[server].setup(config)
 end
 
--- C# (Omnisharp con O mayúscula)
-setup_server("omnisharp", {
-  cmd = { mason_bin .. "Omnisharp" },
-  root_dir = require('lspconfig.util').root_pattern("*.sln", "*.csproj", ".git") or vim.uv.cwd(),
+-- LUA
+setup("lua_ls", {
+  settings = { Lua = { diagnostics = { globals = {'vim'} } } }
+})
+
+-- C# (Omnisharp)
+-- Nota: Omnisharp suele requerir el ejecutable 'omnisharp' en minúsculas en Linux/macOS
+setup("omnisharp", {
+  cmd = { "omnisharp" }, 
   settings = {
     RoslynExtensionsOptions = { enableImportCompletion = true }
   }
 })
 
--- Lua
-setup_server("lua_ls", {
-  cmd = { mason_bin .. "lua-language-server" },
-  settings = { Lua = { diagnostics = { globals = {'vim'} } } }
+-- SQL
+setup("sqlls", {
+  filetypes = { "sql", "mysql" },
+  root_dir = function() return vim.uv.cwd() end,
 })
 
--- Sql
-setup_server("sqlls", {
-    cmd = { mason_bin .. "sql-language-server" },
-    filetypes = { "sql", "mysql" },
-    root_dir = { function(fname)
-		return root_pattern(fname) or vim.loop.os_homedir()
-        end },			
-    settings = { }
-})
-
--- Bash
-setup_server("bash-language-server", {
-    cmd = { mason_bin .. "bash-language-server", "start" },
-    cmd_env = {
-        GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)"
-    },
-    filetypes = { "sh" },
-    root_dir = function(path)
-        if not path or #path == 0 then
-            return
-        end
-        local result = path:gsub(strip_sep_pat, ''):gsub(strip_dir_pat, '')
-        if #result == 0 then
-            return '/'
-        end
-        return result
-    end,
+-- BASH
+setup("bashls", {
+  filetypes = { "sh", "bash" },
 })
 
 -- 4. Opciones de editor
 vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
--- Sincronizar PATH para procesos hijos
-vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
