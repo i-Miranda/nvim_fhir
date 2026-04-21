5d7ed0c7f731d28ff67ef5e3863c19b71dedf0d8
Ivan Miranda <ivan@debian.edu>
2026-04-21 12:16:35 +0200

    added hints to inline errors, updated gitignore

--- init.lua
+++ init.lua
@@ -126,10 +126,10 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/"
 vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
 
--- enable inline diagnostics globally (show errors + warnings)
+-- enable inline diagnostics globally (show errors + warnings + hints)
 vim.diagnostic.config({
 	virtual_text = {
-		severity = { min = vim.diagnostic.severity.WARN },
+		severity = { min = vim.diagnostic.severity.HINT },
 		format = function(d)
 			return d.message
 		end,
